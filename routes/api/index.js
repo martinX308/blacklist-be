d7369ae6525ff2d7bf03919391231a9cc42ab405
Martin <mar.gardemann@gmail.com>
2018-02-28 15:29:37 +0100

    create application API

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,10 +4,12 @@ const express  = require('express');
 const router  = express.Router();
 
 const authentication  = require('./auth');
-const userView  = require('./company-details')
-
+const userView  = require('./company-details');
+const application = require ('./application');
 
 router.use('/auth', authentication);
 router.use('/company-details', userView);
+router.use('/application', application);
+
 
 module.exports = router;
