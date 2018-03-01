11ce1a769c3f8f499bd2b7ebeb9d1f3f5ed51533
Martin <mar.gardemann@gmail.com>
2018-03-01 12:51:03 +0100

    modified blacklist model

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,10 +6,13 @@ const router  = express.Router();
 const authentication  = require('./auth');
 const userView  = require('./company-details');
 const application = require ('./customer-apps');
+const blacklist = require ('./blacklist');
+
 
 router.use('/auth', authentication);
 router.use('/company-details', userView);
 router.use('/customer-apps', application);
+router.use('/fraud-case', blacklist);
 
 
 module.exports = router;
