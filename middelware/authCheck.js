974afe70bd1e857cbc80527c8068e3995aeba43c
Martin <mar.gardemann@gmail.com>
2018-03-01 18:09:30 +0100

    add endpoint mylog

--- middelware/authCheck.js
+++ middelware/authCheck.js
@@ -1,10 +1,19 @@
 'use strict';
+const UserApplication = require('../models/user-application');
 
 function authCheck (req, res, next) {
   if (!req.headers.authorization) {
     return res.status(403).json({ error: 'No credentials sent!' });
-  }
-  next();
-};
+  } else {
+  const auth = req.get("authorization");
+  const credentials = new Buffer(auth.split(" ").pop(), "base64").toString("ascii").split(":");
+  UserApplication.findOne( {"apiKey.token":credentials[0],"apiSecret.token":credentials[1]})
+    .then((application) => {
+        if (application === null) {
+          return res.status(403).json({error:"Access Denied (incorrect credentials)"});
+      }
+    next();
+  });
+}}
 
 module.exports = authCheck;
--- routes/api/blacklist.js
+++ routes/api/blacklist.js
@@ -5,6 +5,7 @@ const authCheck = require ('../../middelware/authCheck');
 
 const BlackistEntry = require('../../models/blacklist-entry');
 const UserApplication = require('../../models/user-application');
+const RequestLog = require('../../models/request-log');
 
 router.post('/add', authCheck,(req,res,next) => {
   const auth = req.get("authorization");
@@ -13,9 +14,6 @@ router.post('/add', authCheck,(req,res,next) => {
   
   UserApplication.findOne( {"apiKey.token":credentials[0],"apiSecret.token":credentials[1]})
     .then((application) => {
-      if (application === null) {
-           return res.status(403).json({error:"Access Denied (incorrect credentials)"});
-      } else {
         const newEntry = BlackistEntry ({
           application:application._id,
           ddNumber
@@ -24,9 +22,8 @@ router.post('/add', authCheck,(req,res,next) => {
         newEntry.save()
         .then(() => {
           console.log(newEntry);
-          res.status(200).send("Dataset was processed successfully");
+          res.status(201).send("Dataset was processed successfully");
         });
-        }
     })    
     .catch(next);
 })
@@ -35,25 +32,38 @@ router.post('/check', authCheck,(req,res,next) => {
   const auth = req.get("authorization");
   const ddNumber = req.body.ddNumber;
   const credentials = new Buffer(auth.split(" ").pop(), "base64").toString("ascii").split(":");
-  
-  UserApplication.findOne( {"apiKey.token":credentials[0],"apiSecret.token":credentials[1]})
-    .then((application) => {
-      if (application === null) {
-           return res.status(403).json({error:"Access Denied (incorrect credentials)"});
-      } else {
-        BlackistEntry.find({"ddNumber":ddNumber})
-          .then((list) => {
-          console.log(list);
-            if(list.length > 0) {
-              res.status(200).json({match:"true"});
-            } else {
-              res.status(200).json({match:"false"});
-            }
-        });
-        }
-    })    
-    .catch(next);
+  const match = false;
 
+  BlackistEntry.find({"ddNumber":ddNumber})
+    .then((list) => {
+      if(list.length > 0) {
+        match = true;
+        res.status(200).json({"match":match});
+      } else {
+        res.status(200).json({"match": match});
+      }
+  })
+  .then( () => {
+    const newRequest = RequestLog({
+      api:credentials[0],
+      response:match
+    });
+    newRequest.save()
+    .then(()=> 
+    console.log (newRequest));
+  });
 })
+
+
+router.get('/mylog',authCheck,(req,res,next) => {
+  const auth = req.get("authorization");
+  const credentials = new Buffer(auth.split(" ").pop(), "base64").toString("ascii").split(":");
+
+  RequestLog.find({api:credentials[0]})
+    .then((requests) => {
+      res.status(200).json(requests);
+    });
+ 
+});
 
 module.exports = router;
