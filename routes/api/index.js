c7dad5c10f793f5df0f7e9bf526f8edee6bf9fa0
Martin <mar.gardemann@gmail.com>
2018-03-08 10:33:47 +0100

    remove blacklist

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,13 +6,13 @@ const router  = express.Router();
 const authentication  = require('./auth');
 const userView  = require('./company-details');
 const application = require ('./customer-apps');
-const blacklist = require ('./blacklist');
+//const blacklist = require ('./blacklist');
 
 
 router.use('/auth', authentication);
 router.use('/company-details', userView);
 router.use('/customer-apps', application);
-router.use('/blacklist-entry', blacklist);
+//router.use('/blacklist-entry', blacklist);
 
 
 module.exports = router;
